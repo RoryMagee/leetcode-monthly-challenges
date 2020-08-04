412af93eff2643dd4dca616d901a5e8236ccdf92
Rory <rory.magee2@gmail.com>
2020-08-04 21:42:09 +0100

    added solution for 04/08 that doesn't use a loop + added question to file

--- aug/04-power-of-four.js
+++ aug/04-power-of-four.js
@@ -1,19 +1,36 @@
+/*
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 5
+Output: false
+Follow up: Could you solve it without loops/recursion?
+*/
+
 var isPowerOfFour = function(num) {
-    if (num === 1) return true;
     const base = 4    
-    let exp = 1;
+    let exp = 0;
     while (true) {
-        let temp = Math.pow(base, exp++); 
+        let temp = base ** exp++; 
         if (temp > num) break;
         if (temp === num) return true;
     } 
     return false;
 }
 
-console.log(isPowerOfFour(16)); //Expect True
-console.log(isPowerOfFour(256)); //Expect True
-console.log(isPowerOfFour(262144)); //Expect True
-console.log(isPowerOfFour(15)); //Expect False
-console.log(isPowerOfFour(1111111)); //Expect False
+const _isPowerOfFour = (num) => {
+    return Number.isInteger(Math.log(num) / Math.log(4));
+}
+
+console.log(_isPowerOfFour(16)); //Expect True
+console.log(_isPowerOfFour(256)); //Expect True
+console.log(_isPowerOfFour(262144)); //Expect True
+console.log(_isPowerOfFour(15)); //Expect False
+console.log(_isPowerOfFour(1111111)); //Expect False
 console.log(isPowerOfFour(1)); //Expect True
 
