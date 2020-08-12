115715cf5b194cc7acd896e7ee0f0d19646085d9
Rory <rory.magee2@gmail.com>
2020-08-12 21:23:49 +0100

    added solution for 12/08

---
+++ aug/12-pascals-triangle.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    if (rowIndex === 0) return [1];
+    let res;
+    function re(arr) {
+        if (arr.length-1 === rowIndex) {
+            res = arr;
+            return;
+        }
+        const toBeAdded = (arr.length-2)+1;
+        let startPointer = 0;
+        let endPointer = 1;    
+        let newArr = [];
+        while (endPointer < arr.length) {
+           newArr.push(arr[startPointer++] + arr[endPointer++]); 
+        }
+        newArr.unshift(1);
+        newArr.push(1);
+        re(newArr);
+    }
+    let startArr = [1,1];
+    re(startArr);
+    console.log(res);
+    return res;
+};
