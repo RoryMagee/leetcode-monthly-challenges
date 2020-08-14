0ee52f0ee9fd0d8cedcaec22c68fa2151bc14379
Rory <rory.magee2@gmail.com>
2020-08-14 20:24:49 +0100

    added solution for 14/08

---
+++ aug/14-longest-palindrome.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    if (s.length === 1) return 1;
+    let obj = {};
+    let total = 0;
+    let singleFound = false;
+    for (let char of s) {
+        if (obj.hasOwnProperty(char)) {
+            obj[char]++;
+        } else {
+            obj[char] = 1;
+        }
+    }
+    console.log(obj);
+    for (let key in obj) {
+        if (obj[key] > 1) { 
+            if (obj[key] % 2 === 0) {
+                total += obj[key];
+            } else {
+                total += obj[key] - 1; 
+                if (!singleFound) {
+                    total++;
+                    singleFound = true;
+                } 
+            } 
+        }
+        if (singleFound === false && obj[key] === 1) {
+            singleFound = true;
+            total++;
+        }
+    } 
+    return total;
+};
