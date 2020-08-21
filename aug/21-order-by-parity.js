f143dca896aff16436738d39c7047ef94e6ff786
Rory <rory.magee2@gmail.com>
2020-08-21 14:47:03 +0100

    updated solution for 21/08 to faster implementation

--- aug/21-order-by-parity.js
+++ aug/21-order-by-parity.js
@@ -1,3 +1,4 @@
+const { performance } = require('perf_hooks');
 /**
  * @param {number[]} A
  * @return {number[]}
@@ -7,3 +8,26 @@ var sortArrayByParity = function(A) {
     let even = A.filter(num => num % 2 === 0);
     return [...even, ...odd];
 };
+
+var sortArrayByParity2 = function(A) {
+    let res = [];
+    for (let i = 0; i < A.length; i++) {
+        if (A[i] % 2 === 0) {
+            res.unshift(A[i]);
+        } else {
+            res.push(A[i]);
+        }
+    }
+    return res;
+};
+
+let input = [123,1,32,645,6,34,12,3,6,7,4,563,41,3, 12,3234,34,5,546,4567,56,7,56,734,523,4,56,58,67,89768,9,67,4,56,345,23,5,3546,54,7,567,567,534,5,4325,36,4556];
+
+console.time('sortArrayByParity');
+sortArrayByParity(input);
+console.timeEnd('sortArrayByParity');
+
+console.time('sortArrayByParity2');
+sortArrayByParity2(input);
+console.timeEnd('sortArrayByParity2');
+
