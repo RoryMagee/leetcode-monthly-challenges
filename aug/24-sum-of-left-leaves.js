049278341d2818df838d5b38d28ca7d44723f89b
Rory <rory.magee2@gmail.com>
2020-08-24 15:49:50 +0100

    updated solution for 24/08

--- aug/24-sum-of-left-leaves.js
+++ aug/24-sum-of-left-leaves.js
@@ -1,26 +1,26 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
+ * definition for a binary tree node.
+ * function treenode(val, left, right) {
  *     this.val = (val===undefined ? 0 : val)
  *     this.left = (left===undefined ? null : left)
  *     this.right = (right===undefined ? null : right)
  * }
  */
 /**
- * @param {TreeNode} root
+ * @param {treenode} root
  * @return {number}
  */
 var sumOfLeftLeaves = function(root) {
+    if (root === null) return 0;
     let total = 0;
     let curr = root;
     const traverse = function(node) {
-        if (node.left) {
+        if (node.left && !node.left?.left && !node?.left?.right) {
             total += node.left.val;
-            traverse(node.left)
         }
+        if (node.left) traverse(node.left);
         if (node.right) traverse(node.right);
     }
-    if (root === null) return 0;
     traverse(curr);
     return total;
 };
