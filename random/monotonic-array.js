a2798fe404c8bc0611921bea03b6eeaa34a37433
Rory <rory.magee2@gmail.com>
2020-09-06 19:07:26 +0100

    updated solution to include problem statement

--- random/monotonic-array.js
+++ random/monotonic-array.js
@@ -1,6 +1,11 @@
 /**
  * @param {number[]} A
  * @return {boolean}
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+
+Return true if and only if the given array A is monotonic.
  */
 var isMonotonic = function(A) {
     if (A.length <= 2) return true;
