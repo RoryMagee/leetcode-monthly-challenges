e1481763648cb420d22ae879fab01402c025cb03
Rory <rory.magee2@gmail.com>
2020-09-22 17:03:58 +0100

    updated solution for 22/09

--- sept/22-majority-element-2.js
+++ sept/22-majority-element-2.js
@@ -2,21 +2,34 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+//var majorityElement = function(nums) {
+//    let o = Math.floor(nums.length/3);
+//    let obj = {};
+//    nums.forEach(i => {
+//        if (obj[i]) {
+//            obj[i]++;
+//        } else {
+//            obj[i] = 1;
+//        }
+//    });
+//    let res = [];
+//    for (let key in obj) {
+//        if (obj[key] > o) {
+//            res.push(key);
+//        }
+//    }
+//    return res;
+//}
+
 var majorityElement = function(nums) {
-    let o = Math.floor(nums.length/3);
     let obj = {};
-    nums.forEach(i => {
-        if (obj[i]) {
-            obj[i]++;
-        } else {
-            obj[i] = 1;
-        }
-    });
     let res = [];
+    nums.forEach(nums => obj[nums] ? obj[nums]++ : obj[nums] = 1);
     for (let key in obj) {
-        if (obj[key] > o) {
-            res.push(key);
-        }
+        if (obj[key] > Math.floor(nums.length / 3)) res.push(key);
     }
+    console.log(res);
     return res;
 }
+
+majorityElement([1,1,1,3,3,2,2,2]);
