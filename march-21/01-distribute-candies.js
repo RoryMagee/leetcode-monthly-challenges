b691f781d101d47ea4e46ea87d1df7b8aa0204e7
rory-magee <rory.magee2@gmail.com>
2021-03-01 21:50:39 +0000

    refactored solution

--- march-21/01-distribute-candies.js
+++ march-21/01-distribute-candies.js
@@ -6,9 +6,5 @@ const distributeCandies = function(candyType) {
     const uniqueCandies = new Set(candyType).size;
     const noOfCandies = candyType.length;
     const aliceCanEat = noOfCandies / 2;
-    let returnValue = aliceCanEat;
-    if (uniqueCandies < returnValue) {
-        returnValue = uniqueCandies;
-    }
-    return returnValue;
+    return Math.min(uniqueCandies, aliceCanEat);
 };
